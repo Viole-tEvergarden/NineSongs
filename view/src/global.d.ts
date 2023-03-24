737a1813fa2d70ebd5545c9e7e7475f5d4807e84
zhangxiaobo9794 <2067838947@qq.com>
2023-03-24 12:01:13 +0800

    api请求初步建立

--- view/src/global.d.ts
+++ view/src/global.d.ts
@@ -1,1 +1,23 @@
+// 主题
 type theme = 'dark' | 'light' | null;
+
+/**
+   * 分页查询参数
+   */
+interface PageQuery {
+  pageNum: number;
+  pageSize: number;
+}
+/**
+   * 分页响应对象
+   */
+interface PageResult<T> {
+  /**
+   * 数据列表
+   */
+  list: T;
+  /**
+   * 数据总数
+   */
+  total: number;
+}
